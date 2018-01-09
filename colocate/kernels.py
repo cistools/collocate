Fix the order the coordinates are passed for correct calculation of distance

colocate/kernels.py
@@ -203,9 +203,9 @@ class nn_horizontal(Kernel):
             raise ValueError
         for idx, data_point in iterator:
             if (haversine(np.asarray([point.longitude.item(), point.latitude.item()]),
-                          nearest_point[['longitude', 'latitude']].values) >
+                          nearest_point[['latitude', 'longitude']].values) >
                     haversine(np.asarray([point.longitude.item(), point.latitude.item()]),
-                              data_point[['longitude', 'latitude']].values)):
+                              data_point[['latitude', 'longitude']].values)):
                 nearest_point = data_point
         return nearest_point.vals
 